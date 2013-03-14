63322e158f54da18c0f17e7068b560039bf2b7ca
Pirkka Esko <pirkka@gmail.com>
2013-03-14 15:17:29 +0200

    rails devise postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deck::Application.config.secret_token = 'a933655a814a1c4d99e1c8c92620b50ae4f658e0b9d0fdb63b94d72a5b276570dad42644cd5f3e57f09808be7a2f3d0f4608d54ad725e3e1697b4803a677222f'
