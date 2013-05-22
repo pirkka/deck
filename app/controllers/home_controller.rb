4aae9844637d5ca64f2a7f92c6723bb0c554e0b2
Pirkka Esko <pirkka@gmail.com>
2013-05-22 16:08:49 +0300

    moar random rotation for cards member area layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @members = Member.all
+    render :layout => 'public'
   end
   
   def invoice
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,22 @@
 module ApplicationHelper
 
   def random_rotation_css
-    rotate = -20 + rand(40)
+    rotate = -16 + rand(32)
     margin_top = -30 + rand(60)
     margin_left = -40 + rand(80)
+<<-END
+transform: rotate(#{rotate}deg);
+-ms-transform: rotate(#{rotate}deg); /* IE 9 */
+-webkit-transform: rotate(#{rotate}deg); /* Safari and Chrome */
+margin-top: #{margin_top}px;
+margin-left: #{margin_left}px;
+END
+  end
+
+  def slight_random_rotation_css
+    rotate = -10 + rand(20)
+    margin_top = -10 + rand(20)
+    margin_left = -20 + rand(40)
 <<-END
 transform: rotate(#{rotate}deg);
 -ms-transform: rotate(#{rotate}deg); /* IE 9 */
